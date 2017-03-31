38dd15659726a137052a0afebb9a6754904bf7c1
Jesse J. Anderson <jessejanderson@gmail.com>
2017-03-31 09:50:51 -0700

    add Event with Agent basics - name, start, end

---
+++ lib/event.ex
@@ -0,0 +1,48 @@
+defmodule Events.Event do
+  # @enforce_keys [:name]
+  defstruct [:datetime_start, :datetime_end, :name, :description, isOvernight: false]
+
+  alias Events.Event
+
+  # Assumed for now
+  @timezone "America/Los_Angeles"
+
+  def start_link(name) do
+    Agent.start_link(fn -> %Event{name: name} end)
+  end
+
+  # Get Current State
+  def name(event), do:           Agent.get(event, fn state -> state.name end)
+  def datetime_start(event), do: Agent.get(event, fn state -> state.datetime_start end)
+  def datetime_end(event), do:   Agent.get(event, fn state -> state.datetime_end end)
+
+  # Setting Datetimes
+  def set_datetime_start(event, datetime), do: set_datetime(event, :start, datetime)
+  def set_datetime_end(event, datetime),   do: set_datetime(event, :end, datetime)
+
+  defp set_datetime(event, :start, %DateTime{} = datetime) do
+    Agent.update(event, &(%Event{&1 | datetime_start: datetime}))
+  end
+
+  defp set_datetime(event, :end, %DateTime{} = datetime) do
+    Agent.update(event, &(%Event{&1 | datetime_end: datetime}))
+  end
+
+  defp set_datetime(event, start_or_end, {{_y, _mo, _d}, {_h, _mi, _s}} = datetime_erl) do
+    {:ok, datetime} = Calendar.DateTime.from_erl(datetime_erl, @timezone)
+    set_datetime(event, start_or_end, datetime)
+  end
+
+  # Convenience Printing
+  def puts(event), do: Event.to_string(event) |> IO.puts
+
+  def to_string(event) do
+    name = event |> Event.name
+    datetime_start = event |> Event.datetime_start |> Event.format_datetime_for_print
+    datetime_end = event |> Event.datetime_end |> Event.format_datetime_for_print
+    "Event: #{name}\nStart: #{datetime_start}\nEnd: #{datetime_end}"
+  end
+
+  def format_datetime_for_print(nil), do: ""
+  def format_datetime_for_print(datetime), do: Calendar.DateTime.Format.rfc850(datetime)
+end
