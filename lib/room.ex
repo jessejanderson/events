c91ef36639f0e6d75ec26428acf2a2f796963a42
Jesse J. Anderson <jessejanderson@gmail.com>
2017-04-02 23:40:28 -0700

    pass Room tests and some cleanup

--- lib/room.ex
+++ lib/room.ex
@@ -14,15 +14,25 @@ defmodule Events.Room do
     GenServer.start_link(__MODULE__, name)
   end
 
+  def name(event),      do: GenServer.call(event, :name)
+  def events(event),    do: GenServer.call(event, :events)
+  def approvers(event), do: GenServer.call(event, :approvers)
+
+  def set_name(event, name) do
+    GenServer.call(event, {:set_name, name})
+  end
+
   def add_event(room, event) do
     GenServer.call(room, {:add_event, event})
+  end
+
+  def add_approver(approver, event) do
+    GenServer.call(approver, {:add_approver, event})
   end
 
   def list_events(room) do
     GenServer.call(room, :list_events)
   end
-
-  def wtf(room), do: GenServer.call(room, :wtf)
 
   # +-------------------+
   # | C A L L B A C K S |
@@ -32,9 +42,25 @@ defmodule Events.Room do
     {:ok, %Room{name: name}}
   end
 
+  def handle_call(:name, _from, state),      do: {:reply, state.name, state}
+  def handle_call(:events, _from, state),    do: {:reply, state.events, state}
+  def handle_call(:approvers, _from, state), do: {:reply, state.approvers, state}
+
+  def handle_call({:set_name, name}, _from, state) do
+    %Room{state | name: name}
+    |> reply_tuple
+  end
+
   def handle_call({:add_event, event}, _from, state) do
-    new_state = %Room{state | events: [event|state.events]}
-    {:reply, new_state, new_state}
+    events = [event|state.events] |> List.flatten
+    %Room{state | events: events}
+    |> reply_tuple
+  end
+
+  def handle_call({:add_approver, approver}, _from, state) do
+    approvers = [approver|state.approvers] |> List.flatten
+    %Room{state | approvers: approvers}
+    |> reply_tuple
   end
 
   def handle_call(:list_events, _from, state) do
@@ -42,7 +68,14 @@ defmodule Events.Room do
     {:reply, events, state}
   end
 
-  def handle_call(:wtf, _from, state) do
-    {:reply, state, state}
-  end
+  # +---------------+
+  # | P R I V A T E |
+  # +---------------+
+
+  defp reply_tuple(state), do: {:reply, state, state}
+
+  # +-----------------------+
+  # | C O N V E N I E N C E |
+  # +-----------------------+
+
 end
